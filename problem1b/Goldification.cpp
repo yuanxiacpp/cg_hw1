258259af75d87087b45f9763c48f1bc0101ffc20
Yuan Xia <yuan.xia.cpp@gmail.com>
2014-01-19 01:27:02 -0500

    milestone 1 checkin

--- problem1b/Goldification.cpp
+++ problem1b/Goldification.cpp
@@ -10,6 +10,49 @@ using namespace std;
 const unsigned int max_h = 2000;
 const unsigned int max_w = 2000;
 
+const double yellow_start = 45.0;
+const double yellow_end = 65.0;
+const double blue_start = 200.0;
+const double blue_end = 255.0;
+
+double getHue(int r, int g, int b) {
+  int mx = max(r, max(g, b));
+  int mi = min(r, min(g, b));
+  
+  double h = 0.0;
+  
+  if (mx == r && g >= b)
+    h = 60 * (double)(g-b)/(mx-mi);
+  else if (mx == r && g < b)
+    h = 60 * (double)(g-b)/(mx-mi) + 360;
+  else if (mx == g)
+    h = 60 * (double)(b-r)/(mx-mi) + 120;
+  else if (mx == b)
+    h = 60 * (double)(r-g)/(mx-mi) + 240;
+
+  return h;
+}
+bool isYellow(double h) {
+  if (yellow_start <= h && h <= yellow_end)
+    return true;
+  return false;
+}
+bool isBlue(double h) {
+  if (blue_start <= h && h <= blue_end)
+    return true;
+  return false;
+}
+
+
+
+void updateYellow(int &r, int &g, int &b) {
+  return;
+}
+
+void updateBlue(int &r, int &g, int &b) {
+  return;
+}
+
 int main(int argc, char* argv[]) {
   
   if (argc != 3) {
@@ -31,8 +74,31 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < w; i++) {
       int idx = 3*(i*h+j);
       
+      int r = image[idx];
+      int g = image[idx+1];
+      int b = image[idx+2];
+      
+      double h = getHue(r, g, b);
+      
+      if (isYellow(h)) {
+	updateYellow(r, g, b);
+	image[idx] = r;
+	image[idx+1] = g;
+	image[idx+2] = b;
+      }
+      else if (isBlue(h)) {
+	updateBlue(r, g, b);
+	image[idx] = r;
+	image[idx+1] = g;
+	image[idx+2] = b;
+      }
 
     }
+
+  if (!save_tiff(argv[2], image, w, h)) {
+    cerr << "Could not write output file " << argv[2] << endl;
+    return 1;
+  }
 
 
 
