1a415b8920928c04a0eddd6f2757655676857271
Yuan Xia <yuan.xia@yale.edu>
2014-01-25 16:53:14 -0500

    problem4a start

---
+++ problem4a/create_hybrid.m
@@ -0,0 +1,11 @@
+function [result] = create_hybrid(low_file, high_file)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
+function [low, high] = hybrid_split(image)
+    
+end
+
