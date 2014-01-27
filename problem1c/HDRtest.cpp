939808bcba39b0a461fa25dec410def70348c046
Yuan Xia <yuan.xia@yale.edu>
2014-01-27 00:37:36 -0500

    finish problem1c

--- problem1c/HDRtest.cpp
+++ problem1c/HDRtest.cpp
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <sstream>
 
 #include "figio.h"
 
 using namespace std;
+
+const double PI = 3.1415926;
+
+void printVector(vector<vector<double> > &v, int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++)
+      printf("%7.1f ", v[i][j]);
+    cout << endl;
+  }
+  return;
+      
+}
+int outputToTiff(vector<vector<double> > &v, string &str, int size);
 
 int main(int argc, char* argv[]) {
   
@@ -13,33 +28,89 @@ int main(int argc, char* argv[]) {
     cerr << "Usage: " << argv[0] << " <size> " << endl;
     return 1;
   }
+
+  int size = atoi(argv[1]);
+  vector<vector<double> > hdr(size, vector<double>(size, 0.0));
+
+  for (int i = 0; i < size; i++) 
+    for (int j = 0; j < size; j++) 
+      hdr[i][j] = 1000.0 * sin(2*PI*j/size);
+
+  int start = 7*size/16;
+  int end = 9*size/16;
+
+  for (int i = start; i <= end; i++)
+    for (int j = start; j <= end; j++)
+      hdr[i][j] = 10000.0;
+
   
-  unsigned char* image = new unsigned char[max_w*max_h*3];
-  int w = max_w;
-  int h = max_h;
 
-  if (!load_tiff(argv[1], image, w, h)) {
-    cout << "Could not read input file " << argv[1] << endl;
-    return 1;
-    
-  }
+  vector<vector<double> > linear(size, vector<double>(size, 0.0));
 
-  for (int j = 0; j < h; j++)
-    for (int i = 0; i < w; i++) {
-      int idx = 3*(j*w+i);
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      linear[i][j] = 255.0/11000.0 * hdr[i][j] + 255.0/11.0;
+
+  
+
+  vector<vector<double> > log_result(size, vector<double>(size, 0.0));
+  
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      log_result[i][j] = 255.0 * log(1001+hdr[i][j]) / log(11001);
       
-      int r = image[idx];
-      int g = image[idx+1];
-      int b = image[idx+2];
-      
+  
+
+  //printVector(linear, size);
+
+  //cout << "=========================================" << endl;
+  //printVector(log_result, size);
 
 
-  if (!save_tiff(argv[2], image, w, h)) {
-    cerr << "Could not write output file " << argv[2] << endl;
-    return 1;
-  }
+  string str1 = "linear-";
+  string str2 = "log-";
+  outputToTiff(linear, str1, size);
+  outputToTiff(log_result, str2, size);
+  
+  
 
+  
 
 
   return 0;
 }
+
+int outputToTiff(vector<vector<double> > &v, string &str, int size) {
+  stringstream s;
+  s << size;
+  string filename = str + s.str() + ".tiff";
+  
+  unsigned char *image = new unsigned char[size*size*3];
+  
+
+  for (int j=0; j < size; j++)
+    for (int i = 0; i < size; i++) {
+      int idx = 3 * (j*size + i);
+      char val = (char)v[j][i];
+      
+      image[idx] = val;
+      image[idx+1] = val;
+      image[idx+2] = val;
+    }
+
+  if (!save_tiff(filename.c_str(), image, size, size)) {
+    cerr << "Could not write output file " << filename << endl;
+
+    return 1;
+  }
+  return 0;
+  
+}
+
+
+
+
+
+
+
+
