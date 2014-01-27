5f87a4d947bd7db23f1aa07db2808a928fea2f3f
Yuan Xia <yuan.xia@yale.edu>
2014-01-26 22:46:55 -0500

    result of problem3

--- problem3/show_fourier.m
+++ problem3/show_fourier.m
@@ -2,6 +2,12 @@ function [ result ] = show_fourier(image_name)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     
+    % reference: http://courses.engr.illinois.edu/cs498dh/fa2011/projects/hybrid/ComputationalPhotography_ProjectHybrid.html
+    % 1. read in image and change it to gray
+    % 2. do fast fourier transformation
+    % 3. shift fourier 
+    % 4. obtain the log of the abs value
+    % 5. scale data and display object
     result = imagesc(log(abs(fftshift(fft2(rgb2gray(imread(image_name)))))));
     
     %imwrite(result, 'output.jpg');
